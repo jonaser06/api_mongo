d2c012213f1264bdbd932fc4e5049506250ffd22
Jonathan Narvaez <jonaser06@gmail.com>
2020-02-11 22:43:21 -0500

    route

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+header('Access-Control-Allow-Origin:*'); 
+header('Access-Control-Allow-Headers:X-Request-With');
+
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
 ini_set('date.timezone', 'America/Lima');
 error_reporting(E_ALL);
 ini_set('display_errors', true);
